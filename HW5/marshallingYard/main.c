db0a258450160220e7f2abbd3e055c1127bb8c18
michaelharlam <palptin2014@gmail.com>
2024-10-30 22:18:08 +0300

    The last working version of the program

--- HW5/marshallingYard/main.c
+++ HW5/marshallingYard/main.c
@@ -18,21 +18,34 @@ void marshallingYard(char *infix, char *postfix) {
     Stack *stack = createStack();
     int i = 0, j = 0;
 
-    while (postfix[i] != '\0') {
+    while (infix[i] != '\0') {
         if (infix[i] >= '0' && infix[i] <= '9') {
             postfix[j] = infix[i];
             ++j;
         } else if (infix[i] == '(') {
             push(stack, infix[i]);
         } else if (infix[i] == ')') {
-            while (!isEmpty(stack) && pop(stack) != '(') {
-                postfix[j] = pop(stack);
+            while (!isEmpty(stack)) {
+                char element = pop(stack);
+                if (element == '(') {
+                    break;
+                }
+                postfix[j] = element;
                 ++j;
             }
         } else {
-            while (!isEmpty(stack) && operationsPriority(infix[i]) <= operationsPriority(stack)) {
-                postfix[j] = pop(stack);
-                ++j;
+            if (!isEmpty(stack)) {
+                char operator = pop(stack);
+                push(stack, operator);
+                while (!isEmpty(stack) && operationsPriority(infix[i]) <= operationsPriority(operator)) {
+                    postfix[j] = pop(stack);
+                    ++j;
+                    if (isEmpty(stack)) {
+                        break;
+                    }
+                    operator = pop(stack);
+                    push(stack, operator);
+                }
             }
             push(stack, infix[i]);
         }
@@ -46,9 +59,11 @@ void marshallingYard(char *infix, char *postfix) {
 }
 
 int main(void) {
-    char infix[100];
-    char postfix[100];
+    char infix[100] = { 0 };
+    char postfix[100] = { 0 };
+    printf("Вас приветствует сортировочная станция!\n\nВведите выражение в инфиксной форме без пробелов: ");
     scanf("%s", infix);
     marshallingYard(infix, postfix);
+    printf("Выражение в постфиксной форме: ");
     printf("%s\n", postfix);
 }
