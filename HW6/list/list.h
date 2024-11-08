2c1be337e115f876731be1342de9e799f75c9ca3
michaelharlam <palptin2014@gmail.com>
2024-11-08 14:44:19 +0300

    version of list for merge sort s Please enter the commit message for your changes. Lines starting

--- HW6/list/list.h
+++ HW6/list/list.h
@@ -1,8 +1,30 @@
-//
-// Created by Михаил Харламов on 06.11.2024.
-//
-
 #ifndef LIST_H
 #define LIST_H
+
+typedef struct Node Node;
+
+typedef struct List List;
+
+List* initList(void);
+
+Node* createNode(char *name, char *telephoneNumber);
+
+size_t insertAfter(List *list, Node* node, Node* newNode);
+
+size_t insertBefore(List *list, Node *node, Node *newNode);
+
+size_t insertInBeginning(List *list, char *name, char *telephoneNumber);
+
+size_t append(List *list, char *name, char *telephoneNumber);
+
+Node* removeFromList(List *list, Node *node);
+
+Node *next(Node *node);
+
+Node *first(List *list);
+
+Node *last(List *list);
+
+char *getValue(List *list, Node *node, int part);
 
 #endif //LIST_H
