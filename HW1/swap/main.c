3b5265011500b17c53d9d773b3a82ef9134a355d
michaelharlam <palptin2014@gmail.com>
2024-11-10 01:51:34 +0300

    shit

--- HW1/swap/main.c
+++ HW1/swap/main.c
@@ -1,3 +1,20 @@
-//
-// Created by Михаил Харламов on 10.11.2024.
-//
+#include <stdio.h>
+#include <assert.h>
+
+void swap(int *number1, int *number2) {
+    if (*number1 == *number2) {
+        return;
+    }
+    *number1 ^= *number2;
+    *number2 ^= *number1;
+    *number1 ^= *number2;
+}
+
+int main(void) {
+    int number1 = 1, number2 = 2;
+    printf("Put the numbers\n");
+    int result = scanf("%d%d", &number1, &number2);
+    assert(result == 2);
+    swap(&number1, &number2);
+    printf("a = %d, b = %d\n", number1, number2);
+}
