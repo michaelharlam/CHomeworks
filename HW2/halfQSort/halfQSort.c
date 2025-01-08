9af6f9d0f4f4f303cdf5a71b410f9ff0d97906d7
michaelharlam <120271217+michaelharlam@users.noreply.github.com>
2025-01-08 09:21:41 +0300

    Revert "Homework 2: halfQSort"

--- HW2/halfQSort/halfQSort.c
+++ HW2/halfQSort/halfQSort.c
@@ -3,64 +3,35 @@
 #include <stdbool.h>
 
 void swap(int *number1, int *number2) {
-    if (number1 == number2) {
-        return;
-    }
     *number1 ^= *number2;
     *number2 ^= *number1;
     *number1 ^= *number2;
 }
 
-void halfQSort(int *array, int size) {
-    int left = 0;
-    int right = size - 1;
-    int middle = array[0];
-
-    swap(&array[0], &array[size / 2]);
+void halfQSort(int array[], int length) {
+    int firstElementIndex = 0;
+    int counter = 0;
 
     do {
-        while (array[left] < middle) {
-            ++left;
+        counter = 0;
+        for (int i = 0; i < length; ++i) {
+            if ((array[i] < array[firstElementIndex]) && (i > firstElementIndex)) {
+                swap(&array[i], &array[firstElementIndex]);
+                firstElementIndex = i;
+                ++counter;
+            }
+            if ((array[i] > array[firstElementIndex]) && (i < firstElementIndex)) {
+                swap(&array[i], &array[firstElementIndex]);
+                firstElementIndex = i;
+                ++counter;
+            }
         }
-        while (array[right] > middle) {
-            --right;
-        }
-        if (left <= right) {
-            swap(&array[left], &array[right]);
-            ++left;
-            --right;
-        }
-    } while (left <= right);
+    } while (counter != 0);
 }
 
-int userInput(void) {
-    printf("Program is ready for work!\n");
-    printf("Input the length of array:\n");
-    int length = 0;
-    scanf("%d", &length);
-    int *array = malloc(length * sizeof(int));
-    if (array == NULL) {
-        printf("Memory allocation error!\n");
-        return 1;
-    }
-    for (int i = 0; i < length; ++i) {
-        array[i] = rand() % length;
-    }
-    printf("First element: %d\n", array[0]);
-    halfQSort(array, length);
-    for (int i = 0; i < length; ++i) {
-        printf(" %d", array[i]);
-    }
-    free(array);
-    return 0;
-}
-
-bool testOfHalfQSort(void) {
+bool test1(void) {
     int array[10] = {5, 1, 7, 2, 3, 8, 3, 90, 4};
-    int firstElement = 5;
-    int element = 0;
-    int length = 10;
-
+    int firstElement = 5, element = 0, length = 10;
     halfQSort(array, length);
     while (array[element] != firstElement) {
         if (array[element] > firstElement) {
@@ -80,9 +51,20 @@ bool testOfHalfQSort(void) {
 }
 
 int main(void) {
-    if (!testOfHalfQSort()) {
-        return -1;
+    if (test1()) {
+        printf("Program is ready for work!\n");
+        printf("Input the length of array:\n");
+        int length = 0;
+        scanf("%d", &length);
+        int* array = malloc(length * sizeof(int));
+        for (int i = 0; i < length; ++i) {
+            array[i] = rand() % length;
+        }
+        printf("First element: %d\n", array[0]);
+        halfQSort(array, length);
+        for (int i = 0; i < length; ++i) {
+            printf(" %d", array[i]);
+        }
+        free(array);
     }
-
-    return userInput();
 }
